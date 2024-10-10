e27055b9050fcfc0496bba98bd243e09e2200c81
David <davixer02@gmail.com>
2024-10-10 12:11:06 -0500

    crear sesion

--- api/src/backend/signup.php
+++ api/src/backend/signup.php
@@ -9,14 +9,28 @@ $pass=$_POST['passwd'];
 //encrypt password using md5 hash algorithm
 $enc_pass = md5($pass);
 
+//validate if email all ready exists
+$query = "SELECT * FROM users WHERE email = '$email'";
+$result = pg_query($conn, $query);
+$row = pg_fetch_assoc($result);
+if ($row) {
+    echo "<script>alert('Email already exists!')</script>";
+    header('refresh:0;url=http://127.0.0.1/beta/api/src/register_form.html');
+    exit();
+}
+//query to insert data into users table
 $query = "insert into users (email, password) 
 values('$email', '$enc_pass')";
 
 $result = pg_query($conn, $query);
-
-if($result){
-    echo "Registration Successful!";
-}else{echo "Registration Failure";}
+//execute 
+if($result) {
+    //echo "Registration susccesful!";
+    echo"<script>alert('Registration susccesful!')</script>";
+    header('refresh:0;url=http://127.0.0.1/beta/api/src/login_form.html');
+} else {
+    echo "Registration failed!";
+}
 
 pg_close($conn);
 ?>
